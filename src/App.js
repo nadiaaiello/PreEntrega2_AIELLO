a6ae90cee41412771c6a5a7d795ac944f1e589b3
nadiaaiello <nadialucianaaiello@gmail.com>
2023-10-09 20:17:59 -0300

    pre entrega 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-    <header><Navbar/></header>
+    <header>
+      <Navbar/>
+      <ItemListContainer greeting={'Bienvenidos'}/>
+    </header>
+
   );
 }
 
